a9291db73b2687ea00050914cd975b1c3053fbb8
DeN2828 <den-tu@ua.fm>
2022-10-22 13:42:45 +0300

    Version 1.0.0

---
+++ braking distance calculation/braking distance calculation/Calculate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace braking_distance_calculation
+{
+    internal class Calculate
+    {
+        float k { get; set; }
+        float f { get; set; }
+        float v { get; set; }
+        float s { get; set; }
+
+        public Calculate(float k, float f, float v)
+        {
+            this.k = k;
+            this.f = f;
+            this.v = v;
+            s = 0F;
+        }
+
+        public string Calculates()
+        {
+            s = (k / (254*f)) * v*v;
+            return $"Тормозной путь = {s} meters";
+        }
+
+        
+    }
+}
---
+++ braking distance calculation/braking distance calculation/Program.cs
@@ -0,0 +1,4 @@
+﻿using braking_distance_calculation;
+
+Calculate calculate = new Calculate(1F,0.2F,100);
+Console.WriteLine(calculate.Calculates());
